2bb7f096507d10e8289eaa1e4e7e95e2eff10aa2
Peter Vera <petercvera@gmail.com>
2026-02-16 09:43:52 -0800

    add linting

---
+++ infrastructure/eslint-config/eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      // TypeScript-specific rules
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      
+      // General rules
+      'no-console': 'off', // Allow console in apps
+    },
+  },
+  {
+    ignores: ['**/node_modules/**', '**/dist/**', '**/build/**', '**/*.config.js'],
+  }
+);
